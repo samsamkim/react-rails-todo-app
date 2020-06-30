579cdc8f44a03e92d4f8ac2e400434472f13bfda
samsamkim <hoonshik10@gmail.com>
2020-06-30 11:57:31 -0700

    todo items update destroy create

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
     root "pages#my_todo_items", as: :authenticated_root
   end
   root 'pages#home'
-  get 'pages/my_todo_items'
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :todo_items, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
